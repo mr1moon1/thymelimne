da3daf3d0ef2e50bfd2aa27be5c4669df0a52b39
r-pixels <79231868+r-pixels@users.noreply.github.com>
2021-04-24 22:21:32 -0400

    tentwentyone commit

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ from anystring2date import anystring2date
 
 app = Flask(__name__)
 Bootstrap(app)
-file_path = os.path.abspath(os.getcwd())+"\database.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'Heroku_Database_URL'
+file_path = os.path.abspath(os.getcwd(),"database.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 app.config['SECRET_KEY'] = 'meow'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
